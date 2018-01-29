361477aabfedae00d7445f8537e6f016d5d51290
Mario Florence <mflorence1989@gmail.com>
2018-01-28 21:50:36 -0500

    add nav-links on the homepage

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -2,10 +2,11 @@ module DefaultPageContent
 extend ActiveSupport::Concern
 
 included do
-  before_action :set_source
+  before_action :set_page_defaults
 end
 
-def set_title
+def set_page_defaults
   @page_title = "CryptoNet"
+  @seo_keywords = "Mario Florence BitcoinWatcher"
 end
 end
