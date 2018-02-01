7b528734e2d9e601e05d47aa6d6345d5eb70a878
Mario Florence <mflorence1989@gmail.com>
2018-02-01 14:41:01 -0500

    create twitter parser for tweet display

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require "dotenv-rails"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,11 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Bitcoin
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.eager_load_paths << "#{Rails.root}/lib"
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
