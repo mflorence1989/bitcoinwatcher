a2d263fb47b8b306317ecbb03d2f9e0798080192
Mario Florence <mflorence1989@gmail.com>
2018-02-07 12:06:05 -0500

    update secret base call

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(*Rails.groups)
 module Bitcoin
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   end
 end
